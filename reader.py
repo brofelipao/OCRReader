ff5d956e4bda36775aa5f9e1b331aba5430fe045
Luiz Monteiro <luiz.monteiro@senarms.org.br>
2023-02-23 15:43:09 -0400

    salvando em txt

--- reader.py
+++ reader.py
@@ -19,8 +19,9 @@ erosion = cv2.erode(opening, kernel, iterations = 1)
 blurred = cv2.GaussianBlur(erosion, (5, 5), 0)
 thresh = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
-#cv2.imshow('img', thresh)
-#cv2.waitKey(0)
+cv2.imshow('img', thresh)
+cv2.waitKey(0)
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\luiz.monteiro\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 result = pytesseract.image_to_string(thresh, config=config)
-print(filtra_texto(result))
+with open('captcha.txt', 'w') as arq:
+    arq.write(filtra_texto(result))
